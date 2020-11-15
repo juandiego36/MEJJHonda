58e9be7013318cda8af94feb72d89ed6c828d0cb
juandiego36 <35387609+juandiego36@users.noreply.github.com>
2020-11-15 15:39:47 -0600

    Login

--- MejjHonda/MejjHonda/Controllers/LoginController.cs
+++ MejjHonda/MejjHonda/Controllers/LoginController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Linq;
 using MejjHonda.Models;
 
 namespace MejjHonda.Controllers
@@ -21,12 +20,30 @@ namespace MejjHonda.Controllers
         {
             using (MejjHondaEntities db = new MejjHondaEntities()) {
                 var empleadoDetails = db.MEJJ_Empleado.Where(x => x.Mail == empleadoModel.Mail && x.Contraseña == empleadoModel.Contraseña).FirstOrDefault();
-                if (empleadoDetails == null) {
+                if (empleadoDetails == null)
+                {
                     empleadoModel.LoginErrorMessage = "Contrasena o correo incorrectos";
                     return View("Index", empleadoModel);
                 }
+                else {
+                    UserInfo userInfo = new UserInfo();
+
+                    userInfo.Mail = empleadoModel.Mail;
+                    userInfo.Nombre = empleadoModel.Nombre;
+                    userInfo.IdEmpleado = empleadoDetails.IdEmpleado;
+
+                    Session["userInfo"] = userInfo;
+                    return RedirectToAction("Index","Home");
+
+                    ; }
             }
-                return View();
+            return View();
         }
+    }
+
+    public class UserInfo{
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Mail { get; set; }
     }
 }
