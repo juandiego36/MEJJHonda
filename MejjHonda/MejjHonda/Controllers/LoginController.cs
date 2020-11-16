ec2d3152e249c54f998815b466b8d204c8148cf2
juandiego36 <35387609+juandiego36@users.noreply.github.com>
2020-11-15 18:45:43 -0600

    a

--- MejjHonda/MejjHonda/Controllers/LoginController.cs
+++ MejjHonda/MejjHonda/Controllers/LoginController.cs
@@ -26,23 +26,25 @@ namespace MejjHonda.Controllers
                     return View("Index", empleadoModel);
                 }
                 else {
-                    UserInfo userInfo = new UserInfo();
 
-                    userInfo.Mail = empleadoModel.Mail;
-                    userInfo.Nombre = empleadoModel.Nombre;
-                    userInfo.IdEmpleado = empleadoDetails.IdEmpleado;
 
-                    Session["userInfo"] = userInfo;
+                    Session["IdEmpleado"] = empleadoDetails.IdEmpleado;
+                    Session["Nombre"] = empleadoDetails.Nombre;
+                    
+
+                    Console.Write(Session["IdEmpleado"].ToString());
+                    Console.Write(empleadoModel.Nombre);
+
                     return RedirectToAction("Index","Home");
-
-                    ; }
+                }
             }
+        }
+
+        public ActionResult LogOut() {
+            Session.Abandon();
+            return RedirectToAction("Index");
+
         }
     }
 
-    public class UserInfo{
-        public int IdEmpleado { get; set; }
-        public string Nombre { get; set; }
-        public string Mail { get; set; }
-    }
 }
